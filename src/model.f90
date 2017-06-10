ca254d428b9865d7f1df8049f2d465cd9983a86d
simomarsili <simomarsili@gmail.com>
2017-06-10 16:07:55 +0200

    model.f90 minor style changes

--- src/model.f90
+++ src/model.f90
@@ -9,41 +9,39 @@ module model
   private
 
   public :: prm,grd
-
   public :: model_initialize
   public :: model_set_myv
   public :: model_put_myv
   public :: model_collect_prm
   public :: compute_pseudo_likelihood
-
   public :: cond_likelihood
   public :: etot
   public :: fields
   public :: couplings
 
-  ! index of outcome variables
-  integer :: myv
+  ! index of outcome variable (all the others nv - 1 variables are included in the set of explanatory variables)
+  integer :: out_var
   
-  real(kflt), allocatable :: fields(:,:)      ! fields array (ns x nv)
-  real(kflt), allocatable :: couplings(:,:,:) ! couplings array (ns x ns x nv(nv-1)/2)
+  real(kflt), allocatable :: fields(:,:)      ! fields (ns x nv)
+  real(kflt), allocatable :: couplings(:,:,:) ! couplings (ns x ns x nv(nv-1)/2)
 
   ! arrays for fixed residue/sequence
-  real(kflt), allocatable, save :: my_fields(:) ! NS
-  real(kflt), allocatable, save :: my_couplings(:,:,:) ! NS x NV x NS
-  real(kflt), allocatable, save :: my_p1(:) ! NS 
-  real(kflt), allocatable, save :: my_p2(:,:,:) ! NS x NV x NS
-  real(kflt), allocatable, save :: my_f1(:) ! NS 
-  real(kflt), allocatable, save :: my_f2(:,:,:) ! NS x NV x NS
+  real(kflt), allocatable, save :: vfields(:)        ! out_var fields array (ns)
+  real(kflt), allocatable, save :: vcouplings(:,:,:) ! out_var couplings (ns x nv x ns)
+  real(kflt), allocatable, save :: model_f1(:)       ! model single-variable frequencies (ns)
+  real(kflt), allocatable, save :: model_f2(:,:,:)   ! model frequencies for pairs of variables (ns x nv x ns)
+  real(kflt), allocatable, save :: data_f1(:)        ! data single-variable frequencies (ns)
+  real(kflt), allocatable, save :: data_f2(:,:,:)    ! data frequencies for pairs of variables (ns x nv x ns)
 
   ! regularization parameters 
   real(kflt) :: regularization_strength=0.01_kflt ! regularization strength; the default is l2 with regularization_strength=0.01
 
-  ! "cost" functions
+  ! "cost" function-related variables
   real(kflt) :: cond_likelihood,ereg,etot
 
-  ! gradients arrays
-  real(kflt), allocatable, save :: grd(:) ! NS + NS x NV x NS
-  real(kflt), allocatable, save :: prm(:) ! NS + NS x NV x NS
+  ! parameters and gradients 1D arrays for the optimization routines
+  real(kflt), allocatable, save :: prm(:) ! 1D array of parameters (ns + ns x nv x ns)
+  real(kflt), allocatable, save :: grd(:) ! 1D gradient array (ns + ns x nv x ns)
 
 contains
 
@@ -55,12 +53,12 @@ contains
 
     allocate(fields(ns,nv),stat=err)
     allocate(couplings(ns,ns,nv*(nv-1)/2),stat=err)
-    allocate(my_f1(ns),stat=err)
-    allocate(my_f2(ns,nv,ns),stat=err)
-    allocate(my_fields(ns),stat=err)
-    allocate(my_couplings(ns,nv,ns),stat=err)
-    allocate(my_p1(ns),stat=err)
-    allocate(my_p2(ns,nv,ns),stat=err)
+    allocate(data_f1(ns),stat=err)
+    allocate(data_f2(ns,nv,ns),stat=err)
+    allocate(vfields(ns),stat=err)
+    allocate(vcouplings(ns,nv,ns),stat=err)
+    allocate(model_f1(ns),stat=err)
+    allocate(model_f2(ns,nv,ns),stat=err)
     allocate(grd(ns + ns*ns*nv),stat=err)
     allocate(prm(ns + ns*ns*nv),stat=err)
 
@@ -72,29 +70,29 @@ contains
 
   subroutine model_zero()
     real(kflt), parameter :: small_number=1.e-2_kflt
-    my_p1 = 0.0_kflt
-    my_p2 = 0.0_kflt
+    model_f1 = 0.0_kflt
+    model_f2 = 0.0_kflt
     cond_likelihood = 0.0_kflt
     etot = 0.0_kflt
     ereg = - regularization_strength * (sum(prm(:ns)**2) + 0.5_kflt * sum(prm(ns+1:)**2))
   end subroutine model_zero
 
-  subroutine model_set_myv(iv,err) ! my_couplings
+  subroutine model_set_myv(iv,err) ! vcouplings
     use data, only: nd,data_samples,w
     integer, intent(in) :: iv
-    ! make my_couplings given myv 
+    ! make vcouplings given out_var 
     ! must be called before looping on data
     integer :: id,jv,mys
     integer :: err
     integer, allocatable :: list(:)
 
-    myv = iv
-    my_p1 = 0.0_kflt
-    my_p2 = 0.0_kflt
-    my_f1 = 0.0_kflt
-    my_f2 = 0.0_kflt
-    my_fields = 0.0_kflt
-    my_couplings = 0.0_kflt       
+    out_var = iv
+    model_f1 = 0.0_kflt
+    model_f2 = 0.0_kflt
+    data_f1 = 0.0_kflt
+    data_f2 = 0.0_kflt
+    vfields = 0.0_kflt
+    vcouplings = 0.0_kflt       
     cond_likelihood = 0.0_kflt
     etot = 0.0_kflt
     ereg = 0.0_kflt
@@ -103,15 +101,15 @@ contains
 
     ! compute variable-specific arrays of frequencies
     allocate(list(nv),stat=err)
-    my_f1 = 0.0_kflt
-    my_f2 = 0.0_kflt
+    data_f1 = 0.0_kflt
+    data_f2 = 0.0_kflt
     do id = 1,nd
        list = data_samples(:,id)
-       mys = list(myv)
-       my_f1(mys) = my_f1(mys) + w(id)
+       mys = list(out_var)
+       data_f1(mys) = data_f1(mys) + w(id)
        do jv = 1,nv
-          if(jv /= myv) then 
-             my_f2(list(jv),jv,mys) = my_f2(list(jv),jv,mys) + w(id)
+          if(jv /= out_var) then 
+             data_f2(list(jv),jv,mys) = data_f2(list(jv),jv,mys) + w(id)
           end if
        end do
     end do
@@ -120,33 +118,33 @@ contains
   end subroutine model_set_myv
 
   subroutine model_put_myv 
-    ! store my_fields in fields 
-    ! store my_couplings in couplings
+    ! store vfields in fields 
+    ! store vcouplings in couplings
     ! must be called before looping on data
     integer :: jv,k
 
-    fields(:,myv) = my_fields
+    fields(:,out_var) = vfields
     
-    !      --myv--
+    !      --out_var--
     !      x x x x
     !   |  1 x x x
     !  jv  2 4 x x
     !   |  3 5 6 x
     !
-    ! lower triangle packing: jv > myv
+    ! lower triangle packing: jv > out_var
 
     ! remove gauge before adding to couplings
     call model_gauge()
 
-    fields(:,myv) = my_fields
-    do jv = myv+1,nv ! jv > myv
-       k = (myv - 1) * nv - myv * (myv + 1) / 2 + jv 
-       couplings(:,:,k) = couplings(:,:,k) + my_couplings(:,jv,:)
+    fields(:,out_var) = vfields
+    do jv = out_var+1,nv ! jv > out_var
+       k = (out_var - 1) * nv - out_var * (out_var + 1) / 2 + jv 
+       couplings(:,:,k) = couplings(:,:,k) + vcouplings(:,jv,:)
     end do
 
-    do jv = 1,myv-1 ! myv > jv: submatrices must be transposed 
-       k = (jv - 1) * nv - jv * (jv + 1) / 2 + myv
-       couplings(:,:,k) =  couplings(:,:,k) + transpose(my_couplings(:,jv,:))
+    do jv = 1,out_var-1 ! out_var > jv: submatrices must be transposed 
+       k = (jv - 1) * nv - jv * (jv + 1) / 2 + out_var
+       couplings(:,:,k) =  couplings(:,:,k) + transpose(vcouplings(:,jv,:))
     end do
 
   end subroutine model_put_myv
@@ -156,11 +154,11 @@ contains
     real(kflt) :: mat(ns,ns),arr(ns),marr
     real(kflt) :: rsum(ns),csum(ns),totsum
 
-    arr = my_fields
+    arr = vfields
     marr = sum(arr) / real(ns)
     arr = arr - marr
     do jv = 1,nv
-       mat = my_couplings(:,jv,:)
+       mat = vcouplings(:,jv,:)
        totsum = sum(mat)
        totsum = totsum / real(ns*ns)
        do is = 1,ns
@@ -169,15 +167,15 @@ contains
        end do
        rsum = rsum / real(ns)
        csum = csum / real(ns)
-       if(jv /= myv) arr = arr + csum - totsum
+       if(jv /= out_var) arr = arr + csum - totsum
        do js = 1,ns
           do is = 1,ns
              mat(is,js) = mat(is,js) - rsum(is) - csum(js) + totsum
           end do
        end do
-       my_couplings(:,jv,:) = mat
+       vcouplings(:,jv,:) = mat
     end do
-    my_fields = arr
+    vfields = arr
     
   end subroutine model_gauge
 
@@ -196,14 +194,14 @@ contains
     do id = 1,nd
        list = data_samples(:,id)
        ww = w(id)
-       mys = list(myv)
+       mys = list(out_var)
        
-       ! loop over the states of myv 
+       ! loop over the states of out_var 
        do is = 1,ns
-          r = my_fields(is) 
+          r = vfields(is) 
           do jv = 1,nv
-             if(myv /= jv) then 
-                r = r + my_couplings(list(jv),jv,is) 
+             if(out_var /= jv) then 
+                r = r + vcouplings(list(jv),jv,is) 
              end if
           end do
           conp(is) = exp(r)
@@ -215,13 +213,13 @@ contains
        cond_likelihood = cond_likelihood + ww * log(conp(mys))
        
        ! update histograms 
-       ! loop over the states of myv 
+       ! loop over the states of out_var 
        do is = 1,ns
           pp = conp(is) * ww
-          my_p1(is) = my_p1(is) + pp 
+          model_f1(is) = model_f1(is) + pp 
           do jv = 1,nv
-             if(myv /= jv) then 
-                my_p2(list(jv),jv,is) = my_p2(list(jv),jv,is) + pp
+             if(out_var /= jv) then 
+                model_f2(list(jv),jv,is) = model_f2(list(jv),jv,is) + pp
              end if
           end do
        end do
@@ -251,26 +249,26 @@ contains
 
     dim = ns*ns*nv
     ! compute the gradient
-    my_p1 = my_p1 - my_f1 
-    my_p2 = my_p2 - my_f2 
+    model_f1 = model_f1 - data_f1 
+    model_f2 = model_f2 - data_f2 
 
     ! l2 regularization
-    my_p1 = my_p1 + 2.0_kflt * regularization_strength * my_fields
-    my_p2 = my_p2 + 2.0_kflt * 0.5_kflt * regularization_strength * my_couplings
+    model_f1 = model_f1 + 2.0_kflt * regularization_strength * vfields
+    model_f2 = model_f2 + 2.0_kflt * 0.5_kflt * regularization_strength * vcouplings
 
-    !prm(1:ns) = my_fields
-    !prm(ns+1:) = reshape(my_couplings,(/dim/))
+    !prm(1:ns) = vfields
+    !prm(ns+1:) = reshape(vcouplings,(/dim/))
 
-    grd(1:ns) = my_p1
-    grd(ns+1:) = reshape(my_p2,(/dim/))
+    grd(1:ns) = model_f1
+    grd(ns+1:) = reshape(model_f2,(/dim/))
 
   end subroutine update_gradient
 
   subroutine unpack_parameters()
     ! unpack field and couplings after updating parameters
 
-    my_fields = prm(1:ns) 
-    my_couplings = reshape(prm(ns+1:),(/ns,nv,ns/))
+    vfields = prm(1:ns) 
+    vcouplings = reshape(prm(ns+1:),(/ns,nv,ns/))
     
   end subroutine unpack_parameters
 
