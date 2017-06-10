03f080ad00f63581549df6aff2afa569125d699f
simomarsili <simomarsili@gmail.com>
2017-06-10 14:23:43 +0200

    added constants.f90

---
+++ src/constants.f90
@@ -0,0 +1,9 @@
+! Copyright (C) 2015, 2016, Simone Marsili 
+! All rights reserved.
+! License: BSD 3 clause
+
+module constants
+  use kinds
+  implicit none
+  integer, parameter :: long_string=10000
+end module constants
