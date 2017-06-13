a756092ac77996c5b9b948c48aa565dba70fad93
simomarsili <simomarsili@gmail.com>
2017-06-13 23:14:21 +0200

    scores formats

--- test/test.bash
+++ test/test.bash
@@ -1,6 +1,6 @@
-rm test.txt.scores.mtx;
+rm test.txt.scores;
 ../src/mpl -i test.txt 2>/dev/null ;
-if ! cmp test.txt.scores.mtx test.txt.SCORES.mtx >/dev/null 2>&1; then
+if ! cmp test.txt.scores test.txt.SCORES >/dev/null 2>&1; then
     echo "test failed"
 else
     echo "test ok"
